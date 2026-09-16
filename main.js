25fab5b3a62c5e862c14f8d8bf5b559276c4cc6d
typhosj <typhosj@gmx.de>
2026-09-16 12:36:40 +0200

    Make the type check work with TypeScript 7
    TypeScript 7 removed moduleResolution node10. Resolve as node16 instead,
    which for this CommonJS package picks the axios CJS typings; those have
    no `default` export, so load axios with a plain require (the same object
    at runtime). TypeScript 7 also narrows `let forceIndex = undefined` to
    `undefined`, so the variable gets an explicit JSDoc type.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-const axios = require('axios').default;
+const axios = require('axios');
 const qs = require('qs');
 const Json2iob = require('json2iob');
 
@@ -555,6 +555,7 @@ class Renault extends utils.Adapter {
               data = res.data.data.attributes;
             }
 
+            /** @type {boolean | undefined} */
             let forceIndex = undefined;
             const preferedArrayName = undefined;
 
